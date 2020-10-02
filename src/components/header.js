e0eaf1df6da10352095df7335d5cb17360fc9b2c
Pratham82 <mali.prathamesh82@gmail.com>
2020-10-02 17:58:34 +0530

    Converted pure function

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-
-function Header(){
-  return (
-  <header><h1><i class="fas fa-graduation-cap"id="logo"></i>Edu <span className="head">App</span></h1></header>
-  )
-}
+const Header = () => (
+  <header>
+    <h1>
+      <i className="fas fa-graduation-cap" id="logo"></i>Edu{" "}
+      <span className="head">App</span>
+    </h1>
+  </header>
+);
 
 export default Header;
+
