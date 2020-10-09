8970138712b406bcd69ddebc2a087519062c68d8
rakshit <rdsonnn@gmail.com>
2020-10-09 22:23:57 +0530

    style update and skip feature

--- src/components/QuizMain.js
+++ src/components/QuizMain.js
@@ -8,10 +8,9 @@ export default class Quiz extends Component {
     // initiating the local state
     state = {
         quiestions: {
-            1: ' 1. What US city is known as the "birthplace of jazz"?',
-            2: ' 2. What is the capital of Greece?',
-            3: ' 3. What planet gave birth to Superman?',
-           
+            1: 'What US city is known as the "birthplace of jazz"?',
+            2: 'What is the capital of Greece?',
+            3: 'What planet gave birth to Superman?'
         },
         answers: {
             1: {
@@ -28,14 +27,12 @@ export default class Quiz extends Component {
                 1: 'Krypton',
                 2: 'Mars',
                 3: 'Saturn'
-            },
-          
+            }
         },
         correctAnswers: {
             1: '2',
             2: '1',
-            3: '1',
-            4:'1'
+            3: '1'
         },
         correctAnswer: 0,
         clickedAnswer: 0,
@@ -71,38 +68,60 @@ export default class Quiz extends Component {
 
     render(){
         let { quiestions, answers, correctAnswer, clickedAnswer, step, score } = this.state;
-        
+
         function refreshPage() {
             window.location.reload(false);
           }
-
+       
         return(
             <div className="Content">
                 {step <= Object.keys(quiestions).length ? 
                     (<>
+
+                       <div className="nextbtn">
+                        
+                        <button className="QuestionNum">{step}/{Object.keys(quiestions).length}</button>
+                        </div>
+                      
+
                         <Question
                             question={quiestions[step]}
                         />
-                        <Answer 
+                        <div  className="container">
+                      <div className="center">
+                        <Answer
                             answer={answers[step]}
                             step={step}
                             checkAnswer={this.checkAnswer}
                             correctAnswer={correctAnswer}
                             clickedAnswer={clickedAnswer}
+                           
                         />
+                        </div>
+                        </div>
+                       
+                        
+                        <div className="skipbtn">
                         <button
+                        className="skipQuestion"
+                       
+                        onClick={() => this.nextStep(step)}>Skip</button></div>
+                        
+                        <div className="container">
+                        <div className="nextbtn">
+                        <button 
                         className="NextStep"
                         disabled={
                             clickedAnswer && Object.keys(quiestions).length >= step
                             ? false : true
                         }
-                        onClick={() => this.nextStep(step)}>Next</button>
+                        onClick={() => this.nextStep(step)}>Next</button> </div></div>
                     </>) : (
                         <div className="finalPage">
                             <h1>You have completed the quiz!</h1>
                             <h4>Your score is: {score} of {Object.keys(quiestions).length}</h4>
                             <p>Thank you!</p>
-
+                           
                             <button className="tryagainbtn"  onClick={refreshPage}>TRY AGAIN </button>
                         </div>
                     )
